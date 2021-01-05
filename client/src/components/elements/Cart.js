61f5c7c9df5898014fdf1f6f53ddd09cf0c17fca
Alex <failemotions95@gmail.com>
2021-01-05 21:44:57 +0200

    Making a card component

--- client/src/components/elements/Cart.js
+++ client/src/components/elements/Cart.js
@@ -1,11 +1,33 @@
+import React, { useContext } from "react"
+import GamesContext from "./GamesContext"
 
 const Cart = () => {
+  const { cart } = useContext(GamesContext)
+  console.log(cart)
+
+  // price counting
+  const oneGamePrices = cart.map((game) => game.price * game.quantity)
+  const sumTotal = oneGamePrices.reduce(function (a, b) {
+    return a + b
+  }, 0)
+
+  console.log(sumTotal)
   return (
     <>
       <div>Cart</div>
       <div>Orders:</div>
-      <ul>
-      </ul>
+      {cart.length === 0 && <div>Cart is empty</div>}
+      {cart.map((product) => (
+        <div className="game-item">
+          <img src={"/images/" + product.image} alt="" width="300px" />
+          <h1> {product.title}</h1>
+          <h3>{product.price} $</h3>
+          <h3>quantity :{product.quantity}</h3>
+        </div>
+      ))}
+      <br />
+      <h2>Total price:</h2>
+      <h2>{sumTotal}</h2>
     </>
   )
 }
--- client/src/components/elements/Game.js
+++ client/src/components/elements/Game.js
@@ -1,38 +1,48 @@
-import React, {  useContext } from "react"
-import { useParams } from "react-router";
+import React, { useContext } from "react"
+import { useParams } from "react-router"
 import { StyledGame, StyledGameTitle } from "./../styles/StyledGame"
 import GamesContext from "./GamesContext"
 
 const Game = () => {
-  const { id } = useParams();
-  const {products, cart, setToCart} = useContext(GamesContext);
-  const product = products && products[id - 1];
- 
+  const { id } = useParams()
+  const { products, cart, setToCart } = useContext(GamesContext)
+  const product = products && products[id - 1]
+
   const addGame = (id) => {
-    console.log(`Adding game with id: ${id}`);
-  };
+    // checking for existing obj in array
+    const checkingCart = cart.find(({ title }) => title === product.title)
+    // if array contains same obj then prop.quantity increased by 1
+    if (checkingCart) {
+      product.quantity = product.quantity + 1
+    } else {
+      // create quantity property and new obj to cart
+      product.quantity = 1
+      setToCart(product)
+    }
+
+    console.log(checkingCart)
+    console.log(`Adding game with id: ${id}`)
+  }
 
   return (
     <>
-      {product && 
-      <div>
-      
-        <StyledGameTitle>{product.title}</StyledGameTitle>
-        <StyledGame>
-          <div className="image_item">
-            <img src={"/images/" + product.image} alt="" width="600px" />
-          </div>
-          <div className="info_item">
-            <h4 className="item_desc">{product.description}</h4>
-            <div onClick={() => addGame(id)} className="game_button">
-              <div className="game_button_text">Buy now</div>
-              <div className="game_button_text">{product.price} $</div>
+      {product && (
+        <div>
+          <StyledGameTitle>{product.title}</StyledGameTitle>
+          <StyledGame>
+            <div className="image_item">
+              <img src={"/images/" + product.image} alt="" width="600px" />
             </div>
-          </div>
-        </StyledGame>
-    
-      </div>
-      }
+            <div className="info_item">
+              <h4 className="item_desc">{product.description}</h4>
+              <div onClick={() => addGame(id)} className="game_button">
+                <div className="game_button_text">Buy now</div>
+                <div className="game_button_text">{product.price} $</div>
+              </div>
+            </div>
+          </StyledGame>
+        </div>
+      )}
     </>
   )
 }
